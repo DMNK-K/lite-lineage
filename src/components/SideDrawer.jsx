8e9906b619acf740fb35d4fd826482b13889e95b
DK <76963592+DMNK-K@users.noreply.github.com>
2021-01-25 18:27:38 +0100

    Add X close icon

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
-
+import IconX from '../icons/icon_x.svg';
 class SideDrawer extends Component
 {
     render()
@@ -9,7 +9,9 @@ class SideDrawer extends Component
             <div className="side_drawer">
                 <div className="side_drawer_bar">
                     <h3>{this.props.name}</h3>
-                    <button className="side_drawer_close_button" onClick={this.props.closeAction}>x</button>
+                    <button className="side_drawer_close_button close_button" onClick={this.props.closeAction}>
+                        <img alt="close icon" src={IconX}/>
+                    </button>
                 </div>
                 <div className="side_drawer_content">
                     {this.props.content}
